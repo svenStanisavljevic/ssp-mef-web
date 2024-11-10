fbddcaf37c123665546e6154f58c31bee547f425
sven <svenstanisavljevic@gmail.com>
2024-11-10 19:53:32 +0100

    js test 2

--- index.js
+++ index.js
@@ -13,17 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
         const [entry] = entries
         if (entry.isIntersecting) {
             entry.target.classList.remove("hidden");
+            entry.target.style.opacity = '1';
         } else {
             entry.target.classList.add("hidden");
+            entry.target.style.opacity = '0';
+
         }
     } 
 });
 
-function getGreeting() {
-    return "Dobrodošli na moju stranicu!";
-}
+// function getGreeting() {
+//     return "Dobrodošli na moju stranicu!";
+// }
 
-function displayGreeting() {
-    const greetingMessage = getGreeting(); // Pozivamo funkciju koja vraća tekst
-    document.getElementById('greeting').textContent = greetingMessage; // Prikazujemo vrijednost u elementu
-}
+// function displayGreeting() {
+//     const greetingMessage = getGreeting(); // Pozivamo funkciju koja vraća tekst
+//     document.getElementById('greeting').textContent = greetingMessage; // Prikazujemo vrijednost u elementu
+// }
